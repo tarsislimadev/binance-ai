01a5ebc9577bd59488f9cf3aebd31405ef7671fe
Tarsis <br.tmvdl@gmail.com>
2024-03-09 19:46:23 -0300

    Updating app files

--- src/app/html/index.js
+++ src/app/html/index.js
@@ -3,7 +3,19 @@ import { HTML } from '@brtmvdl/frontend'
 export class Page extends HTML {
   onCreate() {
     super.onCreate()
+    this.append(this.getHTML())
+    this.getKlines()
+  }
 
-    this.setText('Binance AI')
+  getHTML() {
+    const html = new HTML()
+    return html
+  }
+
+  getKlines() {
+    fetch('https://api4.binance.com/api/v3/klines?symbol=USDTBRL&interval=1m')
+      .then(res => res.json())
+      .then(table => console.table(table))
+      .catch(err => console.error(err))
   }
 }
