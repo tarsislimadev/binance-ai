92810aaa3981b1b58ee9b9deaa6262bb0ce520da
Tarsis <br.tmvdl@gmail.com>
2024-04-07 12:00:34 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,31 @@
-const { Server } = require('socket.io')
+const config = require('./config.js')
 
-const app = require('express')()
-const server = require('http').createServer(app)
-const io = new Server(server)
+let running = false
 
-io.on('connection', (socket) => console.log(`socket id: ${socket.id}`))
+const fixDecimal = (num = 0) => +num.toString().replace(/(.)99999.+/, (_, n) => n + 1).replace(/000000.+/, 0)
 
-server.listen(80, () => console.log('server running at 80'))
+const run = (runned = 0) =>
+  fetch(config.url.klines('BNBBRL', '1s', 60)).then(res => res.json())
+    .then((json) => Array.from(json).map(([Kline_Open_Time, Open_Price, High_Price, Low_Price, Close_Price, Volume, Kline_Close_Time, Quote, Trades, Taker_Buy_Base, Taker_Buy_Quote]) => ({ Kline_Open_Time: +Kline_Open_Time, Open_Price: +Open_Price, High_Price: +High_Price, Low_Price: +Low_Price, Close_Price: +Close_Price, Volume: +Volume, Kline_Close_Time: +Kline_Close_Time, Quote: +Quote, Trades: +Trades, Taker_Buy_Base: +Taker_Buy_Base, Taker_Buy_Quote: +Taker_Buy_Quote, })))
+    .then((res) => Array.from(res).map((kline, ix) => ({ ...kline, Diff: fixDecimal(ix === 0 ? 0 : kline.Close_Price - res[ix - 1].Close_Price) })))
+    .then((res) => Array.from(res).map(({ Diff }) => Diff))
+    .then((res) => console.log(runned, res))
+    .catch(err => console.error(err))
+    .finally(() => running ? setTimeout(run, 1000, ++runned) : null)
+
+const exit = () => process.exit(0)
+
+const stop = () => { running = false }
+
+const start = () => { running = true; run() }
+
+const exec = (instr = '') => {
+  switch (instr.substring(0, 4)) {
+    case 'star': return start()
+    case 'stop': return stop()
+    case 'run1': return run()
+    case 'exit': return exit()
+  }
+}
+
+process.stdin.on('data', (data) => exec(data.toString()))
