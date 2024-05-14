8bd4e7084601a2d9fbfe63ca47fc2d0eaeabfe94
Tarsis <br.tmvdl@gmail.com>
2024-05-14 10:58:01 -0300

    Updating app files

---
+++ src/api/apis/binance.js
@@ -0,0 +1,17 @@
+const { API } = require('./api.js')
+
+class BinanceAPI extends API {
+  // debug = true
+
+  _request(method = 'GET', pathname = '/', { query = {} } = {}) {
+    return fetch(`https://api4.binance.com${pathname}?${(new URLSearchParams(query)).toString()}`, { method }).then((res) => res.json())
+  }
+
+  klines(symbol = 'BNBBRL', interval = '1s') {
+    this._request('GET', '/api/v3/klines', { query: { symbol, interval, limit: 100 } })
+      .then((json) => this._response('klines', json))
+      .catch((err) => this._error(err))
+  }
+}
+
+module.exports = { BinanceAPI }
