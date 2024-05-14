df308d965d365bcd18d885f5fe779ce2c8d5a3e9
Tarsis <br.tmvdl@gmail.com>
2024-05-14 10:31:19 -0300

    Updating app files

--- src/api.latest/index.js
+++ src/api.latest/index.js
@@ -6,13 +6,7 @@ const { Database } = require('@brtmvdl/database')
 
 const db = new Database({ type: 'fs', config: '/data' })
 
-db.addListener('writeMany', (...data) => console.log('writeMany', ...data))
-
-let running = []
-
 let binance = new BinanceAPI('BNBBRL', '1s', 100)
-
-// binance.on()
 
 binance.on('updated', (...data) => console.log('updated', ...data))
 
@@ -26,13 +20,7 @@ const ee = new EventEmitter()
 
 ee.on('run', (...data) => binance.getKlines(...data))
 
-ee.on('start', (...data) => running.push(setInterval(() => ee.emit('run', ...data), 1000)))
-
-ee.on('stop', (id = running.pop()) => clearInterval(id))
-
 ee.on('exit', () => process.exit(0))
-
-ee.on('running', () => console.log(...running))
 
 ee.on('config', (symbol, interval = '1s', limit = 1000) => binance = new BinanceAPI(symbol, interval, limit))
 
