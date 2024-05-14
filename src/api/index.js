2d9ae570ed05b6258e77f182652a01fadd64d39d
Tarsis <br.tmvdl@gmail.com>
2024-05-14 11:23:06 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,19 @@
 const EventEmitter = require('events')
 const { BinanceAPI } = require('./apis/binance.js')
+const { Database } = require('@brtmvdl/database')
 
 const ee = new EventEmitter()
 const binance_api = new BinanceAPI()
+const db = new Database({ type: 'fs', config: '/data' })
+
+binance_api.on('klines', (arr) => {
+  Array.from(arr)
+    .map(([KlineOpenTime, OpenPrice, HighPrice, LowPrice, ClosePrice, Volume, KlineCloseTime, QuoteAssetVolume, NumberofTrades, TakerBuyBaseAssetVolume, TakerBuyQuoteAssetVolume]) => ({ KlineOpenTime, OpenPrice, HighPrice, LowPrice, ClosePrice, Volume, KlineCloseTime, QuoteAssetVolume, NumberofTrades, TakerBuyBaseAssetVolume, TakerBuyQuoteAssetVolume }))
+    .map((kline) => [kline, db.in('klines').new().writeMany(kline)]).map((kline) => console.log('saved kline', kline))
+})
 
 ee.on('exit', () => process.exit(0))
 
-ee.on('klines', () => binance_api.klines())
+ee.on('klines', (symbol = 'BNBBRL', interval = '1s', limit = 100) => binance_api.klines(symbol, interval, limit))
 
 process.stdin.on('data', (data) => ee.emit(...data.toString().split(/\s+/ig).filter((p) => p)))
