46ef198e71a312597dd15d4206bf3467c37a86b4
Tarsis <br.tmvdl@gmail.com>
2024-05-14 10:38:25 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,7 @@
-console.log('binance ai')
+const EventEmitter = require('events')
+
+const ee = new EventEmitter()
+
+ee.on('exit', () => process.exit(0))
+
+process.stdin.on('data', (data) => ee.emit(...data.toString().split(/\s+/ig).filter((p) => p)))
