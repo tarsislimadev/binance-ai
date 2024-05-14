a7667da26b5afa09bb4a145c72be29d43620b826
Tarsis <br.tmvdl@gmail.com>
2024-05-14 10:58:25 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,11 @@
 const EventEmitter = require('events')
+const { BinanceAPI } = require('./apis/binance.js')
 
 const ee = new EventEmitter()
+const binance_api = new BinanceAPI()
 
 ee.on('exit', () => process.exit(0))
+
+ee.on('klines', () => binance_api.klines())
 
 process.stdin.on('data', (data) => ee.emit(...data.toString().split(/\s+/ig).filter((p) => p)))
