81b3904acf990b774aaf06ab76a700c29066bf1c
Tarsis Lima <br.tmvdl@gmail.com>
2024-12-14 15:42:14 -0300

    Updating app files

--- src/cli/bin/binance.dart
+++ src/cli/bin/binance.dart
@@ -1,11 +1,13 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() async {
-  var symbol = 'BTCBRL';
-  var interval = '1m';
-  var url = Uri.https('api4.binance.com', '/api/v3/klines', {'symbol': symbol, 'interval': interval});
-  
+  var url = Uri.https(
+    'api4.binance.com',
+    '/api/v3/klines',
+    { 'symbol': 'BTCBRL', 'interval': '1m', 'limit': '10' }
+  );
+
   var response = await http.get(url);
-  var jsonResponse = response.body;
-  print('JSON response: $jsonResponse.');
+  print(response.body);
 }
